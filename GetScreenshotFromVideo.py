4d3cffdfba4d74e4d225c697ba9497fbe0a54cd4
TORRENS_PHD_ACCOUNT <TORRENS_PHD_ACCOUNT@NYU>
2024-01-22 12:40:58 -0500

    Fixed some issues with the get screenshotfromvid python script. Added a requirements.txt for environment building

--- GetScreenshotFromVideo.py
+++ GetScreenshotFromVideo.py
@@ -5,14 +5,20 @@ import cv2
 def ScreenshotFromVideo(videoFilename, inputDir, outputDir, atSecond=0.0):
     filename, file_extension = os.path.splitext(videoFilename)
     # Read the video from specified path
-    cam = cv2.VideoCapture(inputDir + videoFilename)
+    videoCaptureName = os.path.join(inputDir, videoFilename)
+    print(f"Attempting to read capture from: {videoCaptureName}")
+    cam = cv2.VideoCapture(videoCaptureName)
     # just cue to 20 sec. position
     cam.set(cv2.CAP_PROP_POS_MSEC,atSecond*1000)
     success,image = cam.read()
     imageFilename = filename + ".png"
     if success:
-        cv2.imwrite(outputDir + imageFilename, image)     # save frame as JPEG file
+        print("Screenshot successfully extracted.")
+        newFilename = os.path.join(outputDir, imageFilename)
+        print(f"Saving file as: {newFilename}")
+        cv2.imwrite(newFilename, image)     # save frame as JPEG file
     else:
+        print("Unable to extract screenshot...")
         imageFilename = None
     cam.release()
     return imageFilename
@@ -29,6 +35,7 @@ def main():
     if len(sys.argv) >= 4:
         outputDir = sys.argv[3]
 
+    print(f"Saving file to: {outputDir}")
     ScreenshotFromVideo(filename, inputDir, outputDir, atSecond)
 
 if __name__ == "__main__":
